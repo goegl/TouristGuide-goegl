0624169d471c2124970c02173a090e986ccb3ec7
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-11 08:55:04 +0100

    Made TouristAttraction.java class just the basics

--- src/main/java/tourism/touristguide/model/TouristAttraction.java
+++ src/main/java/tourism/touristguide/model/TouristAttraction.java
@@ -1,4 +1,51 @@
 package tourism.touristguide.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TouristAttraction {
+    private String name;
+    private String description;
+    private String city;
+    private List<String> tags;
+
+    public TouristAttraction(String name, String description, String city) {
+        this.name = name;
+        this.description = description;
+        this.city = city;
+        this.tags = new ArrayList<>();
+    }
+    public TouristAttraction(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
