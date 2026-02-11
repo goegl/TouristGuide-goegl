ad993c98ab9a9fb6c080135cf957b4afe6b289b3
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-11 12:01:29 +0100

    implemented addAttraction.html

--- src/main/java/tourism/controller/TouristController.java
+++ src/main/java/tourism/controller/TouristController.java
@@ -2,37 +2,52 @@ package tourism.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tourism.model.TouristAttraction;
 import tourism.service.TouristService;
 
 @Controller
 @RequestMapping("/attractions")
 public class TouristController {
-private final TouristService service;
+    private final TouristService service;
 
-    public TouristController(TouristService service){
+    public TouristController(TouristService service) {
         this.service = service;
     }
 
     @GetMapping
-    public String listAttractions(Model model){
+    public String listAttractions(Model model) {
         model.addAttribute("attractions", service.getAllAttractions());
         return "attractionList";
     }
+
     @GetMapping("/{name}")
-    public String findAttractionByName(@PathVariable String name, Model model){
+    public String findAttractionByName(@PathVariable String name, Model model) {
         model.addAttribute("attraction", service.findByName(name));
         return "findAttractionByName";
     }
 
     @GetMapping("/{name}/tags")
-    public String showTags(@PathVariable String name, Model model){
+    public String showTags(@PathVariable String name, Model model) {
         TouristAttraction attraction = service.findByName(name);
         model.addAttribute("attraction", attraction);
         return "tags";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("attraction", new TouristAttraction());
+
+        model.addAttribute("cities", service.getCities());
+        model.addAttribute("tags", service.getTags());
+
+        return "addAttraction";
+    }
+
+    @PostMapping("/save")
+    public String saveAttraction(@ModelAttribute TouristAttraction attraction) {
+        service.add(attraction);
+        return "redirect:/attractions";
     }
 
 }
