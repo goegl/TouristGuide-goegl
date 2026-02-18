579948283d9ea220f6db31d887e1fafd51ece5f7
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-02-18 10:20:17 +0100

    made a test to findAttractionByName

--- src/test/java/tourism/controller/TouristControllerTest.java
+++ src/test/java/tourism/controller/TouristControllerTest.java
@@ -3,16 +3,20 @@ package tourism.controller;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tourism.model.TouristAttraction;
 import tourism.service.TouristService;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TouristController.class)
 class TouristControllerTest {
@@ -33,13 +37,29 @@ class TouristControllerTest {
 
     @Test
     void shouldListAttractions() throws Exception {
-        mockMvc.perform(get("/attractions")).
-                andExpect(status().isOk()).
-                andExpect(view().name("attractionList"));
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attractionList"));
     }
 
     @Test
-    void findAttractionByName() {
+    void shouldFindAttractionByName() throws Exception {
+        TouristAttraction mock = new TouristAttraction(/*"HoolaHoopRink", "yay", "Grenaa", List.of("tag")*/);
+        when(service.findByName("HoolaHoopRink")).thenReturn(mock);
+
+        mockMvc.perform(get("/attractions/HoolaHoopRink"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("findAttractionByName"))
+                .andExpect(model().attributeExists("attraction"))
+                .andExpect(model().attribute("attraction", mock));
+
+
+//        ArgumentCaptor<TouristAttraction> captor = ArgumentCaptor.forClass(TouristAttraction.class);
+//        TouristAttraction captured = captor.getValue();
+//        assertEquals("HoolaHoopRink", captured.getName());
+//        assertEquals("yay", captured.getDescription());
+//        assertEquals("Grenaa", captured.getCity());
+//        assertNotNull(captured.getTags());
     }
 
     @Test
