d1a9a0bca8d5036bd1d5be25233a630222b22a24
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-03-21 13:01:55 +0100

    Deleted old html files and old fields and old constructor Touristattraction

--- src/main/java/tourism/model/TouristAttraction.java
+++ src/main/java/tourism/model/TouristAttraction.java
@@ -3,32 +3,23 @@ package tourism.model;
 import java.util.List;
 
 public class TouristAttraction {
+    private int id;
     private String name;
     private String description;
-    private String cityOld;
-    private List<String> tags;
-
-    private int id;
     private List<Tag> tagList;
     private City city;
-
     private int city_id;
     private List<Integer> tagIds;
 
-    public TouristAttraction(String name, String description, String city, List<String> tags) {
-        this.name = name;
-        this.description = description;
-        this.cityOld = city;
-        this.tags = tags;
-    }
-    public TouristAttraction(int attractionId,String name, String description, int city_id) {
+    public TouristAttraction(int attractionId, String name, String description, int city_id) {
         this.id = attractionId;
         this.name = name;
         this.description = description;
         this.city_id = city_id;
     }
 
-    public TouristAttraction(){}
+    public TouristAttraction() {
+    }
 
     public String getName() {
         return name;
@@ -46,21 +37,6 @@ public class TouristAttraction {
         this.description = description;
     }
 
-    public String getCityOld() {
-        return cityOld;
-    }
-
-    public void setCityOld(String city) {
-        this.cityOld = city;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
 
     public int getId() {
         return id;
