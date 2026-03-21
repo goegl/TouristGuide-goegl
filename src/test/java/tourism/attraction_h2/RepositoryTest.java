cd77270f48203582c71ec5f74420d3ef7fa43281
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-03-21 14:13:43 +0100

    added h2 dependecy, added h2 sql, added package for h2 tests and made repositoryTest class

---
+++ src/test/java/tourism/attraction_h2/RepositoryTest.java
@@ -0,0 +1,14 @@
+package tourism.attraction_h2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = BEFORE_TEST_METHOD)
+public class RepositoryTest {
+
+}
