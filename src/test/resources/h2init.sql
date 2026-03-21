799cba20ac6254dbe59325a55ff3cf3c4af26069
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-21 15:36:59 +0100

    changed from double to single quotes in testing schema, added init mode never to test properties

--- src/test/resources/h2init.sql
+++ src/test/resources/h2init.sql
@@ -31,24 +31,24 @@ create table attraction_tag
 
 
 INSERT INTO city (name)
-VALUES ("Grenaa"),    -- 1
-       ("København"), -- 2
-       ("Århus"),     -- 3
-       ("Roskilde"),  -- 4
-       ("Slagelse"); -- 5
+VALUES ('Grenaa'),    -- 1
+       ('København'), -- 2
+       ('Århus'),     -- 3
+       ('Roskilde'),  -- 4
+       ('Slagelse'); -- 5
 
 INSERT INTO tag (name)
-VALUES ("Cafe"),          -- 1
-       ("Cats"),          -- 2
-       ("Cozy"),          -- 3
-       ("Sea Creatures"), -- 4
-       ("Acrobatics"); -- 5
+VALUES ('Cafe'),          -- 1
+       ('Cats'),          -- 2
+       ('Cozy'),          -- 3
+       ('Sea Creatures'), -- 4
+       ('Acrobatics'); -- 5
 
 INSERT INTO attraction (name,
                         description,
                         city_id)
-VALUES ("Dolphin Show", "A show with dolphins", 1), -- 1
-       ("Cat Cafe", "A cafe with cats you can pet", 2); -- 2
+VALUES ('Dolphin Show', 'A show with dolphins', 1), -- 1
+       ('Cat Cafe', 'A cafe with cats you can pet', 2); -- 2
 
 
 INSERT INTO attraction_tag (attraction_id, tag_id)
