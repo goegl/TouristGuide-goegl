33a5d3827d4fe56ed84a43a2ecdba00eb157a326
ellidena <85340254+ellidena@users.noreply.github.com>
2026-03-23 10:52:27 +0100

    shouldUpdateAttraction now works

--- src/test/java/tourism/controller/TouristControllerTest.java
+++ src/test/java/tourism/controller/TouristControllerTest.java
@@ -131,27 +131,38 @@ class TouristControllerTest {
         verify(service).findAttractionById(1);
     }
 
-//    @Test
-//    void shouldUpdateAttraction() throws Exception {
-//        TouristAttraction touristAttraction = new TouristAttraction("Tivoli", "En forlystelsespark", "København", List.of("Sjovt", "Klassisk"));
-//        when(service.findByName("Tivoli")).thenReturn(touristAttraction);
-//        mockMvc.perform(post("/attractions/update")
-//                        .param("name", "Tivoli")
-//                        .param("description", "En forlystelsespark")
-//                        .param("city", "Grenaa")
-//                        .param("tags", "Sjovt", "Klassisk"))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/attractions"));
-//        verify(service).update((any(TouristAttraction.class)));
-//
-//        ArgumentCaptor<TouristAttraction> captor = ArgumentCaptor.forClass(TouristAttraction.class);
-//        verify(service).update(captor.capture());
-//        TouristAttraction captured = captor.getValue();
-//        assertEquals("Tivoli", captured.getName());
-//        assertEquals("En forlystelsespark", captured.getDescription());
-//        assertEquals("Grenaa", captured.getCityOld());
-//        assertEquals(List.of("Sjovt", "Klassisk"), captured.getTags());
-//        assertNotNull(captured.getTags());
-//    }
+    @Test
+    void shouldUpdateAttraction() throws Exception {
+        TouristAttraction touristAttraction = new TouristAttraction();
+        touristAttraction.setId(1);
+        touristAttraction.setName("Tivoli");
+        touristAttraction.setDescription("En forlystelsespark");
+        touristAttraction.setCity_id(1);
+        touristAttraction.setTagIds(List.of());
+
+        when(service.findAttractionById(1)).thenReturn(touristAttraction);
+
+        mockMvc.perform(post("/attractions/update")
+                        .param("name", "Opdateret Navn")
+                        .param("description", "Opdateret Beskrivelse")
+                        .param("id","1")
+                        .param("city_id", "3")
+                        .param("tagIds", "1", "2"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/attractions"));
+
+        verify(service).updateAttraction((any(TouristAttraction.class)));
+
+        ArgumentCaptor<TouristAttraction> captor = ArgumentCaptor.forClass(TouristAttraction.class);
+        verify(service).updateAttraction(captor.capture());
+        TouristAttraction captured = captor.getValue();
+
+        assertEquals(1, captured.getId());
+        assertEquals("Opdateret Navn", captured.getName());
+        assertEquals("Opdateret Beskrivelse", captured.getDescription());
+        assertEquals(3, captured.getCity_id());
+        assertEquals(List.of(1,2), captured.getTagIds());
+
+    }
 }
 
