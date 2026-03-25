ed43135770f7b40be2f67292a8f7b43a22a92978
August Skipper <funkskipper@hotmail.com>
2026-03-25 11:03:09 +0100

    Changed SQL from english to danish in data.sql, and added new attractions so we have one for every city, with different combinations of tags

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -6,17 +6,22 @@ VALUES ("Grenaa"),    -- 1
        ("Slagelse"); -- 5
 
 INSERT INTO tag (name)
-VALUES ("Cafe"),          -- 1
-       ("Cats"),          -- 2
-       ("Cozy"),          -- 3
-       ("Sea Creatures"), -- 4
-       ("Acrobatics"); -- 5
+VALUES ("Café"),          -- 1
+       ("Katte"),          -- 2
+       ("Hyggeligt"),          -- 3
+       ("Havdyr"), -- 4
+       ("Akrobatik"); -- 5
 
 INSERT INTO attraction (name,
                         description,
                         city_id)
-VALUES ("Dolphin Show", "A show with dolphins", 1), -- 1
-       ("Cat Cafe", "A cafe with cats you can pet", 2); -- 2
+VALUES ("Delfinshow", "Et show med springende delfiner", 1), -- 1
+       ("Kattecafé", "En café fyldt med katte du kan nusse med", 2), -- 2
+       ("Bæver Workshop", "En sammenkomst hvor vi drøfter hvad vi kan lære af den ædle canadiske bæver", 3), --3
+       ("Tigercirkus", "Verdens førende fremvisning af hvordan man får meget store tigere til at hoppe igennem utroligt små huller", 4),
+       ("Bettinas Vilde Vidunder", "Pop-up Fusion Café hvor man på uforståelig vis har kombineret konceptet af en kattecafé i underetagen med
+        hoppende og springende havoddere på øverste etage, Japan-style. Better see it to believe it!", 5);
+
 
 
 INSERT INTO attraction_tag (attraction_id, tag_id)
@@ -24,4 +29,13 @@ VALUES (1, 4),
        (1, 5),
        (2, 1),
        (2, 2),
-       (2, 3);
+       (2, 3),
+       (3, 3),
+       (3, 4),
+       (4, 2),
+       (4, 5),
+       (5, 1),
+       (5, 2),
+       (5, 3),
+       (5, 4),
+       (5, 5);
